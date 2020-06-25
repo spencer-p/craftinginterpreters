4cef616d788d2b9b7fb4ff40496eed14a23a5e3a
Spencer Peterson <spencerjpeterson@gmail.com>
2020-06-25 15:26:28 -0700

    parsing number comparisions

--- pkg/lox/parse/parse.go
+++ pkg/lox/parse/parse.go
@@ -2,15 +2,15 @@ package parse
 
 import (
 	"github.com/spencer-p/craftinginterpreters/pkg/lox/expr"
-	"github.com/spencer-p/craftinginterpreters/pkg/lox/tok"
+	. "github.com/spencer-p/craftinginterpreters/pkg/lox/tok"
 )
 
 type Parser struct {
-	tokens  []tok.Token
+	tokens  []Token
 	current int
 }
 
-func NewParser(toks []tok.Token) *Parser {
+func NewParser(toks []Token) *Parser {
 	return &Parser{
 		tokens:  toks,
 		current: 0,
@@ -18,11 +18,75 @@ func NewParser(toks []tok.Token) *Parser {
 }
 
 func (p *Parser) AST() expr.Type {
-	return &expr.Literal{1}
-	//return p.expression()
+	return p.expression()
 }
 
 func (p *Parser) expression() expr.Type {
-	// TODO
+	return p.equality()
+}
+
+func (p *Parser) equality() expr.Type {
+	e := p.comparison()
+
+	for p.match(BANG_EQUAL, EQUAL_EQUAL) {
+		op := p.previous()
+		right := p.comparison()
+		e = &expr.Binary{
+			Left:  e,
+			Right: right,
+			Op:    op,
+		}
+	}
+
+	return e
+}
+
+func (p *Parser) comparison() expr.Type {
+	return p.primary()
+}
+
+func (p *Parser) primary() expr.Type {
+	if p.match(NUMBER) {
+		tok := p.previous()
+		return &expr.Literal{tok.Lit}
+	}
+
 	return nil
+}
+
+func (p *Parser) match(types ...TokenType) bool {
+	for _, typ := range types {
+		if p.check(typ) {
+			p.advance()
+			return true
+		}
+	}
+
+	return false
+}
+
+func (p *Parser) check(typ TokenType) bool {
+	if p.atEnd() {
+		return false
+	}
+	return p.tokens[p.current].Typ == typ
+}
+
+func (p *Parser) atEnd() bool {
+	return p.current >= len(p.tokens) || p.peek().Typ == EOF
+}
+
+func (p *Parser) advance() Token {
+	if p.atEnd() == false {
+		p.current += 1
+	}
+	return p.previous()
+}
+
+func (p *Parser) peek() Token {
+	return p.tokens[p.current]
+}
+
+func (p *Parser) previous() Token {
+	return p.tokens[p.current-1]
 }
--- pkg/lox/parse/parse_test.go
+++ pkg/lox/parse/parse_test.go
@@ -1,6 +1,7 @@
 package parse
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -16,23 +17,46 @@ func TestParse(t *testing.T) {
 		want expr.Type
 	}{{
 		in:   "1",
-		want: &expr.Literal{1},
+		want: &expr.Literal{1.0},
 	}, {
 		in: "1 == 2",
 		want: &expr.Binary{
-			Left:  &expr.Literal{1},
-			Right: &expr.Literal{2},
+			Left:  &expr.Literal{1.0},
+			Right: &expr.Literal{2.0},
 			Op: Token{
 				Typ: EQUAL_EQUAL,
 			},
 		},
+	}, {
+		in: "2 != 1",
+		want: &expr.Binary{
+			Left:  &expr.Literal{2.0},
+			Right: &expr.Literal{1.0},
+			Op: Token{
+				Typ: BANG_EQUAL,
+			},
+		},
 	}}
 
-	for _, row := range table {
-		tokens := scan.New(row.in).Tokens() // not too happy about dependency. writing tokens is hard.
-		got := NewParser(tokens).AST()
-		if diff := cmp.Diff(got, row.want); diff != "" {
-			t.Errorf("Parse %q failed (-got, +want): %s", row.in, diff)
+	ignoreTokenTypeFields := cmp.FilterPath(func(path cmp.Path) bool {
+		possibleTokenType := path.Index(-2) // parent of value getting compared
+		if possibleTokenType.Type() == reflect.TypeOf(Token{}) &&
+			path.Last().String() != "Typ" {
+			// parent type is Token and the member type is not Token.Typ
+			return true
+		} else {
+			// Token.Typ is allowed along with anything not in a Token
+			return false
 		}
+	}, cmp.Ignore())
+
+	for _, row := range table {
+		t.Run(row.in, func(t *testing.T) {
+			tokens := scan.New(row.in).Tokens() // not too happy about dependency. writing tokens is hard.
+			got := NewParser(tokens).AST()
+			if diff := cmp.Diff(got, row.want, ignoreTokenTypeFields); diff != "" {
+				t.Errorf("Parse %q failed (-got, +want): %s", row.in, diff)
+			}
+		})
 	}
 }
